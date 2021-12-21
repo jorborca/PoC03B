1726b21623051c886e4e64bf6b353258235994ca
Jorge Alexander Borda Castro <jorge.borda@carvajalservicios.onmicrosoft.com>
2021-12-21 18:47:11 -0500

    Add project files.

---
+++ PoC03B/Shared/FormAction.cs
@@ -0,0 +1,8 @@
+﻿namespace PoC03B.Shared;
+
+public class FormAction
+{
+    public string Id { get; set; }
+    //public Action Action { get; set; }
+    //public Action<Dictionary<string, object>> ActionParams { get; set; }
+}
